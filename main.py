62fbea645bc95c6917b9bc8de02ffb41b396854a
Jan Poonthong <janpoonthong628@gmail.com>
2022-05-22 16:08:51 +0700

    Clean up the code

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import requests
-import config
 import os
 import os.path
-
+import requests
 from reportlab.pdfgen import canvas
 
+import config
 
 headers = {
     "Authorization": f"Bearer {config.auth_token}",
@@ -25,7 +24,6 @@ for i in response.json()["views"]:
         print("Directory ", f"{i['name']}", " already exists")
 
 for i in response.json()["views"]:
-    # isdir = os.path.isdir(i["name"])
     os.chdir(i["name"])
     for j in i["pages"]:
         my_canvas = canvas.Canvas(f"{j['name']}.pdf")
@@ -34,31 +32,4 @@ for i in response.json()["views"]:
         my_canvas.setFont("Helvetica", 12)
         my_canvas.drawString(100, 730, f"Description: {j['text_content']}")
         my_canvas.save()
-
-        if j["parent"] != None:
-            for k in i["pages"]:
-                if j["parent"] == k["id"]:
-                    pass
-                    # print(k["name"])
-                    # print(True)
-                if i["id"] == j["view_id"]:
-                    pass
-                    # print(i["name"], i["id"], j["name"], j["view_id"])
-            # print(f"\t{j['name']} has parent")
-            # print(f"\tName: {j['name']}")
-            # print(f"\tDescription: {j['text_content']}")
-            # print("")
-
-        else:
-            pass
-            # print(f"\tName: {j['name']}")
-            # print(f"\tDescription: {j['text_content']}")
-            # print("")
-
-        # my_canvas = canvas.Canvas(f"{j['name']}.pdf")
-        # my_canvas.setFont("Helvetica", 20)
-        # my_canvas.drawString(100, 750, f"Title: {j['name']}")
-        # my_canvas.setFont("Helvetica", 12)
-        # my_canvas.drawString(100, 730, f"Description: {j['text_content']}")
-        # my_canvas.save()
     os.chdir("../")
